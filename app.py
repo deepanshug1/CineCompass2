2628b76274cc386bf2b99da33b96e8e95a3f09c3
deepanshug1 <devgarg880@gmail.com>
2024-04-30 14:16:26 +0530

    first commit

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import pandas as pd
 from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import json
 import bs4 as bs
 import urllib.request
 import pickle
-import requests
 
 # load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
